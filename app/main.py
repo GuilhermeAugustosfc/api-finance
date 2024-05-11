72cc6a8805e2b7061269bc4b114354813c0b78c4
Rafael Scalzo <rafaelscalzo@rafaelvsm.local>
2024-05-11 05:08:56 -0300

    Adicionando criacao de clientes oauth2 e conexao com mysql

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import speech_recognition as sr
@@ -9,9 +9,18 @@ from transcribe import transcribe_audio
 from gpt3_utils import generate_response_gpt
 from driver.mongo import get_db_mongo
 from model.operacao_model import OperacaoModel
+import database.MySQLDatabase as MySQL
+from database.MySQLDatabase import get_db
+from oauthlib.oauth2 import RequestValidator, WebApplicationServer
+from datetime import datetime, timedelta
+from model.TokenModel import AccessToken, RefreshToken, OAuth2Client
+from sqlalchemy.orm import Session
+from oauthlib.common import generate_token
+from model.TokenModel import generate_client_secret
+
+MySQL.create_tables()
 
 app = FastAPI()
-# Adicionar configuração para aceitar solicitações de qualquer origem.
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Permitir solicitações de qualquer origem
@@ -45,9 +54,9 @@ async def cadastrar_gasto(audio: Audio):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-    db = get_db_mongo()
-    model = OperacaoModel(db)
-    model.criar_operacao(response)
+    # db = get_db_mongo()
+    # model = OperacaoModel(db)
+    # model.criar_operacao(response)
     return response
 
 
@@ -84,6 +93,91 @@ async def create_operacao(operacao: Operacao):
         return {"message": "Operação criada com sucesso"}
     else:
         raise HTTPException(status_code=500, detail="Erro ao criar a operação")
+
+
+# Implementação do OAuth2
+class MyRequestValidator(RequestValidator):
+    def validate_client_id(self, client_id, request, *args, **kwargs):
+        db = MySQL.SessionLocal
+        client = db.query(OAuth2Client).filter(OAuth2Client.client_id == client_id).first()
+        if not client:
+            return False
+        return True
+
+    def validate_redirect_uri(self, client_id, redirect_uri, request, *args, **kwargs):
+        db = MySQL.SessionLocal
+        client = db.query(OAuth2Client).filter(OAuth2Client.client_id == client_id).first()
+        if not client:
+            return False
+        if client.redirect_uri != redirect_uri:
+            return False
+        return True
+
+    def get_default_redirect_uri(self, client_id, request, *args, **kwargs):
+        db = MySQL.SessionLocal
+        client = db.query(OAuth2Client).filter(OAuth2Client.client_id == client_id).first()
+        return client.redirect_uri
+
+    def save_authorization_code(self, client_id, code, request, *args, **kwargs):
+        pass
+
+    def get_authorization_code(self, client_id, code, request, *args, **kwargs):
+        pass
+
+    def invalidate_authorization_code(self, client_id, code, request, *args, **kwargs):
+        pass
+
+    def save_token(self, token, request, *args, **kwargs):
+        pass
+
+    def fetch_token(self, access_token, request, *args, **kwargs):
+        pass
+
+    def validate_bearer_token(self, token, scopes, request):
+        pass
+
+
+oauth2_server = WebApplicationServer(MyRequestValidator())
+
+
+
+@app.post("/oauth/token")
+def token(request, db: Session = Depends(get_db)):
+    uri = 'http://127.0.0.1:8000/oauth/token'
+    headers, body, status = oauth2_server.create_token_response(uri, request.method, request.body, request.headers)
+    return body, status, headers
+
+
+@app.post("/clients/")
+def create_client(client_id: str, redirect_uri: str, db: Session = Depends(get_db)):
+    db_client = db.query(OAuth2Client).filter(OAuth2Client.client_id == client_id).first()
+    if db_client:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client already registered")
+    client_secret = generate_client_secret()
+    new_client = OAuth2Client(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    db.add(new_client)
+    db.commit()
+    db.refresh(new_client)
+    return new_client
+
+
+@app.post("/refresh_token/")
+def create_refresh_token(access_token: str, db: Session = Depends(get_db)):
+    db_access_token = db.query(AccessToken).filter(AccessToken.token == access_token).first()
+    if not db_access_token:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Access token not found")
+
+    # Calculando o tempo de expiração do Refresh Token (por exemplo, 7 dias a partir de agora)
+    expire_at = datetime.utcnow() + timedelta(days=7)
+
+    refresh_token = generate_token()
+
+    db_refresh_token = RefreshToken(token=refresh_token, expire_at=expire_at, access_token_id=db_access_token.id)
+    db.add(db_refresh_token)
+    db.commit()
+    db.refresh(db_refresh_token)
+
+    return {"refresh_token": refresh_token}
 
 
 if __name__ == "__main__":
